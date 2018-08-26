0f5dbd175ee058b03b38a70e2e74da51c27458a6
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-08-26 00:06:41 -0700

    Implement getElementsByClassName with helperfunction

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  /*
+	we can use backtracking
+  */
+  var result = [];
+  helper(result, className, window.document.body);
+  return result;
 };
+
+var helper = function(result, className, current){
+	if(current.classList && current.classList.contains(className)) result.push(current);
+	if(current.childNodes){
+		_.each(current.childNodes, function(node){
+			helper(result, className, node);
+		});
+	}
+};
+
+  
+
